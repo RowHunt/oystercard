956d7a54fce0aead1d5e5558d478bd049e6f62b3
RowHunt <watchlist.rh@gmail.com>
2021-02-02 12:30:52 -0500

    Completed up to question 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,28 @@
-# In order to use public transport
-# As a customer
-# I want money on my card
+class Oystercard
+  attr_reader :balance
+  attr_reader :in_journey
+  LIMIT = 90
+
+  def initialize
+    @balance = 0
+    @in_journey = false
+  end
+
+  def top_up(cash)
+    if @balance + cash > LIMIT then fail "Invalid. The limit is #{LIMIT}" else @balance += cash end
+  end
+
+  def touch_in
+    if @balance < 1 then fail "Insufficient funds." else @in_journey = true end
+  end
+
+  def touch_out
+    @in_journey = false
+    deduct
+  end
+
+  private 
+  def deduct(cash = 1)
+    @balance -= cash
+  end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,43 @@
 require 'oystercard.rb'
 
 describe Oystercard do
+  MONEY = 1
+  describe "balance" do
+    it "displays balance" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+  describe "top_up" do
+    it "adds money to balance" do
+      subject.top_up(MONEY)
+      expect(subject.balance).to eq(MONEY)
+    end
+    it "limits balance" do
+      subject.top_up(Oystercard::LIMIT)
+      expect{subject.top_up(MONEY)}.to raise_error("Invalid. The limit is #{Oystercard::LIMIT}")
+    end
+  end
+  describe "touch_in" do
+    it "card in use" do
+      subject.top_up(MONEY)
+      subject.touch_in
+      expect(subject.in_journey).to eq true
+    end
+    context "card has no balance" do
+      it "card cannot be used" do
+        expect{subject.touch_in}.to raise_error "Insufficient funds."
+      end
+    end
+  end
+  describe "touch_out" do
+    it "card not in use" do
+      subject.touch_out
+      expect(subject.in_journey).to eq false
+    end
+    it "decreases balance at touch_out" do
+      subject.top_up(5)
+      subject.touch_in
+      expect{subject.touch_out}.to change{subject.balance}.by(-1)
+    end
+  end
 end
