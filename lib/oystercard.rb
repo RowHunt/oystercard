40fa57f3b623609b22409194efaebfe3a79d3a04
RowHunt <watchlist.rh@gmail.com>
2021-02-03 21:00:13 -0500

    Completed question 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,35 @@
 class Oystercard
   attr_reader :balance
   attr_reader :in_journey
+  attr_reader :entry_station
+
   LIMIT = 90
+  MIN_FARE = 1
 
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(cash)
     if @balance + cash > LIMIT then fail "Invalid. The limit is #{LIMIT}" else @balance += cash end
   end
 
-  def touch_in
-    if @balance < 1 then fail "Insufficient funds." else @in_journey = true end
+  def touch_in(station)
+    fail "Insufficient funds." if @balance < MIN_FARE
+    @in_journey = true 
+    @entry_station = station
   end
 
   def touch_out
     @in_journey = false
+    @entry_station = nil
     deduct
   end
 
-  private 
-  def deduct(cash = 1)
+  private
+  def deduct(cash = MIN_FARE)
     @balance -= cash
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,7 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
-  describe "top_up" do
+  describe "#top_up" do
     it "adds money to balance" do
       subject.top_up(MONEY)
       expect(subject.balance).to eq(MONEY)
@@ -17,26 +17,32 @@ describe Oystercard do
       expect{subject.top_up(MONEY)}.to raise_error("Invalid. The limit is #{Oystercard::LIMIT}")
     end
   end
-  describe "touch_in" do
+  describe "#touch_in" do
     it "card in use" do
       subject.top_up(MONEY)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject.in_journey).to eq true
     end
     context "card has no balance" do
       it "card cannot be used" do
-        expect{subject.touch_in}.to raise_error "Insufficient funds."
+        expect{subject.touch_in(station)}.to raise_error "Insufficient funds."
       end
     end
+    let(:station){ double :station }
+    it 'stores the station' do
+      subject.top_up(MONEY)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
+    end
   end
-  describe "touch_out" do
+  describe "#touch_out" do
     it "card not in use" do
       subject.touch_out
       expect(subject.in_journey).to eq false
     end
     it "decreases balance at touch_out" do
       subject.top_up(5)
-      subject.touch_in
+      subject.touch_in("station")
       expect{subject.touch_out}.to change{subject.balance}.by(-1)
     end
   end
